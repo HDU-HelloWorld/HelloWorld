15d66ddded00252c97050d09e19152827f69dd0a
Chelsea6 <984703001@qq.com>
2023-04-05 19:07:37 +0800

    1.0

--- src/pages/Uploadd.vue
+++ src/pages/Uploadd.vue
@@ -1,75 +1,107 @@
 <template>
+
   <div class="page">
-    <el-row justify="center">
-      <el-col :span="5" >
-        <span class="items-center">姓名</span>
-        <el-input
-          v-model="input1"
-          class="name"
-          placeholder="请输入你的姓名"
-        />
-      </el-col>
-    </el-row>
-    <el-row justify="center">
-      <el-col :span="5" >
-      <span class="items-center">学号</span>
-        <el-input
-          v-model="input2"
-          class="stdnum"
-          placeholder="请输入你的学号"
-        />
-      </el-col>
-    </el-row>
-    <el-row justify="center" class="department">
-      <el-col :span="5" >
-        <p>部门</p>
-        <el-cascader v-model="value" :options="options" @change="handleChange" />
-      </el-col>
-    </el-row>
-    <el-row justify="center">
-      <el-col span="10">
-        <el-upload
-          class="upload-demo"
-          drag
-          action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-          multiple
-        >
-          <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-          <div class="el-upload__text">
-            Drop file here or <em>click to upload</em>
-          </div>
-          <template #tip>
-          <div class="el-upload__tip">
-            jpg/png files with a size less than 500kb
-          </div>
-          </template>
-        </el-upload>
-      </el-col>
-    </el-row>
+    <el-form
+    ref="ruleFormRef"
+    :model="ruleForm"
+    :rules="rules"
+    :size="formSize">
+      <el-row justify="center" prop="name">
+        <el-col :span="4" >
+          <span class="items-center">姓名</span>
+          <el-input
+            v-model="input1"
+            class="name"
+            placeholder="请输入你的姓名"
+          />
+        </el-col>
+      </el-row>
+      <el-row justify="center" prop="stnum">
+        <el-col :span="4" >
+        <span class="items-center">学号</span>
+          <el-input
+            v-model="input2"
+            class="stdnum"
+            placeholder="请输入你的学号"
+          />
+        </el-col>
+      </el-row>
+      <el-row justify="center" class="department" prop="dep">
+        <el-col :span="4" >
+          <span>部门</span>
+          <el-cascader v-model="value" :options="options" @change="handleChange" />
+        </el-col>
+      </el-row>
+      <el-row justify="center">
+        <el-col :span="4">
+          <el-upload
+            class="upload-demo"
+            drag
+            action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+            multiple
+          >
+            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+            <div class="el-upload__text">
+              Drop file here or <em>click to upload</em>
+            </div>
+            <template #tip>
+            <div class="el-upload__tip">
+              jpg/png files with a size less than 500kb
+            </div>
+            </template>
+          </el-upload>
+        </el-col>
+      </el-row>
+      <el-row justify="center">
+        <el-button type="submit" plain>提交</el-button>
+      </el-row>
+    </el-form>
+    
   </div>
-
-  
-
 </template>
 
 
 
-
-
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive,ref } from 'vue'
 import { UploadFilled } from '@element-plus/icons-vue'
+import type { FormInstance, FormRules } from 'element-plus'
+const formSize = ref('default')
+const ruleFormRef = ref<FormInstance>()
+
+const ruleForm = reactive({
+  name: ' ',
+  stnum:' ',
+  dep:' ',
+
+})
+
+const rules = reactive<FormRules>({
+  name: [
+    { required: true, message: 'Please input your name', trigger: 'blur' },
+    { type:'string',min: 1, max: 6, message: 'Length should be 1 to 6', trigger: 'blur' },
+  ],
+  stnum: [
+    { required: true, message: 'Please input your student number', trigger: 'blur' },
+    { type:'number', len:8, message: 'Length should be 8', trigger: 'blur' },
+  ],
+
+  dep: [
+    {
+      required: true,
+      message: 'Please select Activity department',
+      trigger: 'change',
+    },
+  ],
+})
 
 const input1 = ref('')
 const input2 = ref('')
-
-
 
 const value = ref([])
 
 const handleChange = (value) => {
 console.log(value)
-
 }
 const options = ref([
 {
@@ -94,9 +126,12 @@ const options = ref([
   }
   .el-row {
   margin-bottom: 20px;
-}
+  }
 
- .upload-demo{
-  width: 200px;
- }
+  .el-cascader {
+    position: fixed;
+    display: block;
+    text-align: center;
+    width: 245px;
+  }
 </style>
