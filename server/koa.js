559993348c17a96dee25d2a94fc2a682b5ae1905
Lycoiref <1808347356@qq.com>
2023-04-07 21:53:36 +0800

    暂时注释掉打印机代码

--- server/koa.js
+++ server/koa.js
@@ -73,24 +73,24 @@ let storage = multer.diskStorage({
 let upload = multer({ storage: storage })
 
 // 作业上传
-// router.post('/files', upload.any(), ctx => {
-//     //获得路径
-//     console.log(ctx.request.body)
-//     let filename = getfileName(ctx.request.body)
+router.post('/files', upload.any(), ctx => {
+    //获得路径
+    console.log(ctx.request.body)
+    let filename = getfileName(ctx.request.body)
 
 
-//     if (!fs.existsSync(path.join(dataUrl, filename))) {
-//         fs.mkdirSync(path.join(dataUrl, filename))
-//     }
-//     fs.rename(dataUrl + '/' + ctx.files[0].filename, path.join(dataUrl, filename) + "/" + ctx.files[0].filename, function (err) {
-//         if (err) {
-//             throw err
-//         }
-//     })
+    if (!fs.existsSync(path.join(dataUrl, filename))) {
+        fs.mkdirSync(path.join(dataUrl, filename))
+    }
+    fs.rename(dataUrl + '/' + ctx.files[0].filename, path.join(dataUrl, filename) + "/" + ctx.files[0].filename, function (err) {
+        if (err) {
+            throw err
+        }
+    })
 
-//     ctx.response.body = ctx.request.body.id
-//     ctx.response.status = 200
-// })
+    ctx.response.body = ctx.request.body.id
+    ctx.response.status = 200
+})
 
 // 作业删除
 router.post('/delfile', async ctx => {
@@ -106,42 +106,42 @@ router.post('/delfile', async ctx => {
 })
 
 
-router.get('/print', ctx => {
-    // 唤醒打印机
-    shell.exec('cupsenable DeskJet-1110')
-    // 等待1s后打印
-    setTimeout(() => {
-        console.log("printing...")
-        // 获取./static/upload/下的所有文件名
-        let files = shell.ls('./static/upload')
-        // 打印文件名
-        files.forEach(file => {
-            print(baseURL + file)
-            console.log(file)
-        })
-        // 打印成功后删除文件
-        shell.rm('-rf', './static/upload/*')
-        // print()
-        ctx.body = 'success'
-    }, 1000)
-})
+// router.get('/print', ctx => {
+//     // 唤醒打印机
+//     shell.exec('cupsenable DeskJet-1110')
+//     // 等待1s后打印
+//     setTimeout(() => {
+//         console.log("printing...")
+//         // 获取./static/upload/下的所有文件名
+//         let files = shell.ls('./static/upload')
+//         // 打印文件名
+//         files.forEach(file => {
+//             print(baseURL + file)
+//             console.log(file)
+//         })
+//         // 打印成功后删除文件
+//         shell.rm('-rf', './static/upload/*')
+//         // print()
+//         ctx.body = 'success'
+//     }, 1000)
+// })
 
-router.get('/delete', (req, res) => {
-    shell.rm('-rf', './static/upload/*')
-    ctx.body = 'success'
-})
+// router.get('/delete', (req, res) => {
+//     shell.rm('-rf', './static/upload/*')
+//     ctx.body = 'success'
+// })
 
-router.get('/stop', ctx => {
-    shell.exec('lprm')
-    ctx.body = 'success'
-})
+// router.get('/stop', ctx => {
+//     shell.exec('lprm')
+//     ctx.body = 'success'
+// })
 
-router.post('/files', multer({
-    dest: './static/upload'
-}).any(), ctx => {
-    console.log(ctx.request.files)
-    ctx.body = '上传成功'
-})
+// router.post('/files', multer({
+//     dest: './static/upload'
+// }).any(), ctx => {
+//     console.log(ctx.request.files)
+//     ctx.body = '上传成功'
+// })
 
 
 
