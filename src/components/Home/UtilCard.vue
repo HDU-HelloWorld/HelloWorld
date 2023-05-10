e3385e3537e9bd11e55faaca40d83e0eed2f9854
Lycoiref <1808347356@qq.com>
2023-05-10 16:48:03 +0800

    更改文件结构，并完成了卡片样式

--- src/components/Home/UtilCard.vue
+++ src/components/Home/UtilCard.vue
@@ -1,18 +1,23 @@
 <template>
     <div class="util-card">
         <div class="icon">
-            <div class="icon-svg"></div>
+            <div class="icon-svg">
+                <printer />
+            </div>
         </div>
         <div class="info">
             <div class="title">{{ props.title }}</div>
             <div class="content">{{ props.content }}</div>
-            <div class="footer">{{ props.footer }}</div>
+            <div v-if="props.footer" class="footer">
+                <div class="point-tip available-point"></div>{{ props.footer }}
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 import { defineProps } from 'vue'
+import printer from '../../assets/svg/printer.vue'
 
 const props = defineProps({
     title: {
@@ -25,14 +30,14 @@ const props = defineProps({
     },
     footer: {
         type: String,
-        default: 'Footer'
+        default: ''
     }
 })
 </script>
 
 <style lang="less" scoped>
 .util-card {
-    width: 300px;
+    width: 350px;
     height: 200px;
     background-color: #fff;
     border-radius: 10px;
@@ -48,36 +53,63 @@ const props = defineProps({
         justify-content: center;
 
         .icon-svg {
-            width: 100px;
-            height: 100px;
-            background-color: #eee;
-            border-radius: 50%;
+            width: 80%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 75px;
         }
     }
 
-    .info{
+    .info {
         width: 60%;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
         flex-grow: 1;
-        padding: 20px 0;
-        
+        padding: 40px 0;
+
         .title {
             font-size: 24px;
             font-weight: bold;
         }
-        
+
         .content {
-            font-size: 16px;
+            font-size: 13px;
             line-height: 1.5;
-        }
-        
-        .footer {
-            font-size: 14px;
+            width: 80%;
             color: #999;
         }
-    }
-}
 
+        .footer {
+            font-size: 14px;
+            color: #56e215;
+            text-align: right;
+            padding-right: 20px;
+
+            .point-tip {
+                display: inline-block;
+                width: 10px;
+                height: 10px;
+                border-radius: 50%;
+                background-color: #999999;
+                margin-right: 5px;
+
+                &.available-point {
+                    background-color: #56e215;
+                }
+            }
+        }
+    }
+
+    &:hover {
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+        scale: 1.05;
+        transition: all 0.5s;
+    }
+
+    cursor: pointer;
+    transition: all 0.5s;
+}
 </style>
