c19577c63ce1075cad0fc179e5e0d76e659ff0ae
Lycoiref <1808347356@qq.com>
2023-05-11 22:46:36 +0800

    添加Markdown预览菜单，并添加平滑滚动效果

--- src/pages/Utils/ResourcePage.vue
+++ src/pages/Utils/ResourcePage.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="page">
         <div class="markdown-body" v-html="markdown"></div>
+        <div id="markdown-menu" v-html="markdownMenu"></div>
         <!-- <v-md-editor v-model="markdown" height="100vh"></v-md-editor> -->
     </div>
 </template>
@@ -8,6 +9,7 @@
 <script setup>
 import 'github-markdown-css'
 import axios from 'axios'
+import uslug from 'uslug'
 import MarkdownIt from 'markdown-it'
 import MarkdownItAnchor from 'markdown-it-anchor'
 import MarkdownItTOC from 'markdown-it-toc-done-right'
@@ -15,9 +17,12 @@ import MarkdownItTOC from 'markdown-it-toc-done-right'
 import { onMounted, ref } from 'vue'
 
 const markdownUrl = '/src/assets/markdown/HelloWorld社团资源.md'
+// const markdownUrl = '/src/assets/markdown/全栈开发教程1.md'
 const markdown = ref('')
+const markdownMenu = ref('')
+const uslugify = (s) => uslug(s)
 const md = new MarkdownIt({
-    html: true,
+    html: false,
     linkify: true,
     typographer: true,
 })
@@ -25,25 +30,51 @@ const md = new MarkdownIt({
         permalink: true,
         permalinkBefore: true,
         permalinkSymbol: '#',
+        slugify: uslugify,
     })
-    .use(MarkdownItTOC, { slugify: legacySlugify })
+    .use(MarkdownItTOC, {
+        level: 1,
+        containerId: 'markdown-menu',
+        listType: 'ul',
+        slugify: uslugify,
+        // format: format,
+        callback: function (html, opt) {
+            console.log(opt)
+            markdownMenu.value = html
+        },
+    })
 
 onMounted(async () => {
     const ResourceMd = await axios.get(markdownUrl)
     markdown.value = md.render(ResourceMd.data)
-    console.log(markdown.value)
+    setTimeout(() => {
+        console.log(document.querySelectorAll('a'))
+        for (let anchor of document.querySelectorAll('a')) {
+            anchor.addEventListener('click', (e) => {
+                e.preventDefault()
+                const href = anchor.getAttribute('href')
+                console.log(href)
+                const target = document.querySelector(href)
+                console.log(target)
+                target.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                })
+            })
+        }
+    }, 100)
 })
 
-function legacySlugify(s) {
-    return String(s).slugify().toString()
-}
+// function format(x, htmlencode) {
+//     return `<span>${htmlencode(x)}</span>`
+// }
 
 // const updateMarkdown = () => {
 //     console.log(markdown.value)
 // }
 </script>
 
-<style scoped lang="less">
+<style lang="less">
 .markdown-body {
     box-sizing: border-box;
     min-width: 200px;
@@ -55,6 +86,58 @@ function legacySlugify(s) {
 @media (max-width: 767px) {
     .markdown-body {
         padding: 15px;
+    }
+}
+
+#markdown-menu {
+    position: fixed;
+    top: 20px;
+    right: 0;
+    width: 300px;
+    overflow-y: auto;
+    padding: 20px;
+    background-color: rgba(241, 241, 241, 0.3);
+    border-radius: 5px;
+    // border-left: 1px solid #ddd;
+
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        font-size: 20px;
+        li {
+            margin: 0;
+            padding: 0;
+
+            ul {
+                font-size: 16px;
+                ul {
+                    margin-left: 20px;
+                    font-size: 12px;
+
+                    li {
+                        // 添加小圆点
+                        list-style: disc;
+                    }
+                }
+            }
+        }
+    }
+
+    a {
+        display: block;
+        padding: 5px 0;
+        color: #333;
+        text-decoration: none;
+
+        &:hover {
+            color: #000;
+            background-color: #ddd;
+        }
+    }
+    &.active {
+        color: #000;
+        font-weight: bold;
     }
 }
 </style>
