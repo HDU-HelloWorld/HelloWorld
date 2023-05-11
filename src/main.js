6ee3fad38ed697a475752780aa2b1eda3a03330f
apprehen <3514392356@qq.com>
2023-05-11 17:23:44 +0800

    完成link.vue的单个vue部分

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router/router.js'
-
+import './assets/css/reset.css'
 const app = createApp(App)
 
 app.use(router)
--- src/pages/Link.vue
+++ src/pages/Link.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    这是友情链接页面捏
+    <!-- 这是友情链接页面捏 -->
+    <PersonCard></PersonCard>
   </div>
 </template>
 
 <script setup >
+import PersonCard from '../components/Link/PersonCard.vue';
 import { reactive, ref } from 'vue'
 
 
