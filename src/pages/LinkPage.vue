c4e4365d4999de51000b7fb91e95da08403ef5e6
apprehen <3514392356@qq.com>
2023-05-11 17:45:02 +0800

    更改vue文件的名称

---
+++ src/pages/LinkPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <!-- 这是友情链接页面捏 -->
+        <PersonCard></PersonCard>
+    </div>
+</template>
+
+<script setup>
+import PersonCard from '../components/Link/PersonCard.vue'
+</script>
+<style scoped lang="less"></style>
