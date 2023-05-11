ccbd4a893829131e322907c7915084f3bd8df4d5
Lycoiref <1808347356@qq.com>
2023-05-11 10:54:51 +0800

    完成了prettier和eslint的配置，并避免它们打架

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,24 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    'extends': [
+    extends: [
         'eslint:recommended',
-        'plugin:vue/vue3-essential'
+        'plugin:vue/vue3-essential',
+        'plugin:prettier/recommended',
     ],
-    'overrides': [
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
     },
-    'plugins': [
-        'vue'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    }
+    plugins: ['vue'],
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+    },
 }
