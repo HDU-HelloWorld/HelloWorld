d799b5ecca5f531f14905772d6c5b4c57937962d
Lycoiref <lycoiref@qq.com>
2023-10-08 21:39:03 +0800

    feat: change tab width 4 to 2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,24 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:vue/vue3-essential',
-        'plugin:prettier/recommended',
-    ],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['vue'],
-    rules: {
-        indent: ['error', 4],
-        'linebreak-style': 0,
-        quotes: ['error', 'single'],
-        semi: ['error', 'never'],
-    },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': 0,
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
 }
