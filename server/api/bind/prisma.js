ff29bfa29cbea7fe89192e24dfae870d490cc39d
Lycoiref <lycoiref@qq.com>
2023-10-09 21:44:22 +0800

    fix: 用户无法更改自己的信息

--- server/api/bind/prisma.js
+++ server/api/bind/prisma.js
@@ -1,19 +1,37 @@
 import { PrismaClient } from '@prisma/client'
 
 class bindClient {
-    constructor() {
-        this.prisma = new PrismaClient()
+  constructor() {
+    this.prisma = new PrismaClient()
+  }
+  async createUser(user) {
+    const findUser = await this.prisma.hdu_auto_sign_user.findFirst({
+      where: {
+        user_qq: Number.parseInt(user.QQnum),
+      },
+    })
+    if (findUser) {
+      const newUser = await this.prisma.hdu_auto_sign_user.updateMany({
+        where: {
+          user_qq: Number.parseInt(user.QQnum),
+        },
+        data: {
+          hdu_account: user.stuNum,
+          hdu_password: user.HDUkey,
+        },
+      })
+      return newUser
+    } else {
+      const newUser = await this.prisma.hdu_auto_sign_user.create({
+        data: {
+          user_qq: Number.parseInt(user.QQnum),
+          hdu_account: user.stuNum,
+          hdu_password: user.HDUkey,
+        },
+      })
+      return newUser
     }
-    async createUser(user) {
-        const newUser = await this.prisma.hdu_auto_sign_user.create({
-            data: {
-                user_qq: Number.parseInt(user.QQnum),
-                hdu_account: user.stuNum,
-                hdu_password: user.HDUkey,
-            },
-        })
-        return newUser
-    }
+  }
 }
 
 export default bindClient
